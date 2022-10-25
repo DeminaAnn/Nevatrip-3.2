4868f6c66814a80c535cd17faf07edb73f306329
Anna <novgorodova.anya@gmail.com>
2022-10-26 01:02:58 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,23 +1,39 @@
 "use strict";
 
-let routeAllItems; //все направления
-let newRouteAllItems;
+// routeAllItems; 
+let newRouteAllItems; //все направления нового селектора "обратно" для "туда-обратно"
 let currentRoute; // элемент time
 let routeItem = "из A в B"; // по-умолч направление
-let timeDepart = new Date(); //время отправления
-let timeDepartEnd;
-let timeDepart1; //время отправления туда
+
+//время отправления по-умолч 18-00
+let timeDepart = new Date();
+timeDepart.setHours(18);
+timeDepart.setMinutes(0);
+
+let timeDepart1;
+
+//время прибытия по-умолч 18-50
+let timeDepartEnd = new Date();
+timeDepartEnd.setHours(18);
+timeDepartEnd.setMinutes(50);
+
 let timeDepart2; //время отправления обратно
+
+
 let timeRoute = "50 минут"; // по-умолч время в пути
 let countOfTicket; //кол-во билетов
 let totalPrice; //стоимость билетов
 let priceOfTicket = 700; // по-умолч стоимость в 1 сторону
+
+// вспомогательные штучки
 let clon;
 let hoursTimeDepart1;
 let minTimeDepart1;
+let hoursTimeDepart2;
+let minTimeDepart2;
 
 let today = new Date();
-let newTime;
+let newTimeContainer;
 
 
 
@@ -32,16 +48,12 @@ endContainer[0].after(div);
 
 
 
-
-
 // получаем массив всех времен
-routeAllItems = document.querySelector('#time').querySelectorAll('option');
-//routeAllItems = 
+const routeAllItems = document.querySelector('#time').querySelectorAll('option'); //все направления в одну сторону "туда", "обратно"
 // по-умолчанию видны времена только для направления "из A в B"
-for (let i = 6; i < (routeAllItems.length - 1); ++i) {
+for (let i = 5; i < (routeAllItems.length); ++i) {
    routeAllItems[i].hidden = true;
 }
-
 
 
 
@@ -70,8 +82,8 @@ function clickOnRoute() {
       }
 
       document.querySelector('#newTime') //найти все newTime и удалить
-      if (newTime) {
-         newTime.remove();
+      if (newTimeContainer) {
+         newTimeContainer.remove;
       }
 
       // для "из B в A" _______________________
@@ -82,8 +94,8 @@ function clickOnRoute() {
       for (let i = 0; i < 6; ++i) {
          routeAllItems[i].hidden = true;
       }
-      if (newTime) {
-         newTime.remove();
+      if (newTimeContainer) {
+         newTimeContainer.remove();
       }
       // для "из A в B и обратно в А" _________
 
@@ -94,9 +106,9 @@ function clickOnRoute() {
       }
 
       clon = document.querySelector('#time');
-      newTime = clon.cloneNode(true);
-      newTime.id = "newTime";
-      time.after(newTime);
+      newTimeContainer = clon.cloneNode(true);
+      newTimeContainer.id = "newTime";
+      time.after(newTimeContainer);
 
       newRouteAllItems = document.querySelector('#newTime').querySelectorAll('option');
 
@@ -107,6 +119,42 @@ function clickOnRoute() {
 
       priceOfTicket = 1200;
       timeRoute = "1 час и 40 минут";
+
+
+      const newTime = document.querySelector('#newTime');
+
+      function clickNewTime() {
+         timeDepart2 = document.getElementById('newTime').value;
+         console.log(timeDepart2);
+         hoursTimeDepart2 = +timeDepart2.slice(0, 2);
+         minTimeDepart2 = +timeDepart2.slice(3, 5);
+         timeDepartEnd = new Date();
+         timeDepartEnd.setHours(hoursTimeDepart2);
+         timeDepartEnd.setMinutes(minTimeDepart2);
+         console.log(document.querySelector('#newTime').querySelectorAll('option'));
+         for (let i of document.querySelector('#newTime').querySelectorAll('option')) {
+            if ((timeDepart - timeDepartEnd) <= 3000000) {
+               console.log(timeDepart);
+               console.log(timeDepartEnd);
+               //if (i.value != timeDepart2) {
+               console.log(i.value);
+               i.disabled = 'disabled';
+               //exit;
+               //document.querySelector('#newTime').querySelectorAll('option')[i].disabled = 'disabled';
+
+            }
+         }
+         //console.log(document.getElementsByTagName(document.querySelector('#newTime')));
+         //console.log(newTime.querySelectorAll("[value='timeDepart2']"));
+         document.getElementsByTagName('option')[0].disabled = 'disabled';
+         if ((timeDepart.getMinutes() - timeDepartEnd) < 0) {
+
+            //document.querySelectorAll('[data-foo="timeDepart2"]').style.disabled = true;
+
+         }
+      }
+
+      newTime.addEventListener("click", clickNewTime);
    }
 
 }
@@ -122,10 +170,12 @@ route.addEventListener("click", clickOnRoute);
 
 const time = document.querySelector('#time');
 
+
 function clickTime() {
 
+
+   timeDepart = new Date();
    timeDepart1 = document.getElementById('time').value;
-   //timeDepart1 = document.querySelector('#time').value;
    console.log(timeDepart1);
    hoursTimeDepart1 = +timeDepart1.slice(0, 2);
    minTimeDepart1 = +timeDepart1.slice(3, 5);
@@ -133,19 +183,28 @@ function clickTime() {
    timeDepart.setHours(hoursTimeDepart1);
    timeDepart.setMinutes(minTimeDepart1);
    timeDepartEnd = structuredClone(timeDepart);
-   let temppp = timeDepart;
-   timeDepartEnd.setMinutes(timeDepartEnd.getMinutes() + 50);
+   //let temppp = timeDepart;
 
-   if ((timeDepart - today) <= 0) {
-      console.dir(document.querySelector('[timeDepart1]'));
-      //document.querySelector('[timeDepart1]').setAttribute('disabled', 'disabled');
-      //document.querySelector('#time').setAttribute('disabled', 'disabled');
-      //console.log(document.querySelector('[timeDepart1]').style);
+   if ((routeItem == "из A в B") || (routeItem == "из B в A")) {
+      timeDepartEnd.setMinutes(timeDepartEnd.getMinutes() + 50);
    }
+
+   //if ((timeDepart - today) <= 0) {
+   //console.dir(document.querySelector('[timeDepart1]'));
+   //document.querySelector('[timeDepart1]').setAttribute('disabled', 'disabled');
+   //document.querySelector('#time').setAttribute('disabled', 'disabled');
+   //console.log(document.querySelector('[timeDepart1]').style);
+   //}
 
 }
 
+function clickNewTime() {
+   timeDepart2 = document.getElementById('newTime').value;
+   console.log(timeDepart2);
+}
+
 time.addEventListener("click", clickTime);
+
 
 
 
@@ -173,10 +232,10 @@ button.addEventListener("click", inTotal);
 
 
 
-//function clickChoise() {
+//function clickChoice() {
 //   total.innerHTML = "";
 //   newTime.remove();
 
 //}
 
-//choiseParam.onclick = clickChoise;
+//choiceParam.onclick = clickChoice;
